b9ca153b4a336e25309aed13097ff6a9511f67eb
X17-jy <3927204180@qq.com>
2026-06-11 03:41:44 +0000

    Add 数据分析在线教育平台：首页、课程、项目、Pandas实战10项目

--- app.py
+++ app.py
@@ -4,10 +4,15 @@
 """
 
 from flask import Flask, render_template
-from flask_frozen import Freezer
 
 app = Flask(__name__)
-freezer = Freezer(app)
+
+try:
+    from flask_frozen import Freezer
+    freezer = Freezer(app)
+    HAS_FREEZER = True
+except ImportError:
+    HAS_FREEZER = False
 
 # 课程数据
 courses = [
@@ -70,35 +75,39 @@ def index():
     return render_template('index.html', courses=courses[:4], projects=projects[:3])
 
 
+@app.route('/courses')
 @app.route('/courses.html')
 def courses_page():
     """课程页"""
     return render_template('courses.html', courses=courses)
 
 
+@app.route('/projects')
 @app.route('/projects.html')
 def projects_page():
     """项目页"""
     return render_template('projects.html', projects=projects)
 
+@app.route('/pandas-projects')
 @app.route('/pandas-projects.html')
 def pandas_projects():
     """Pandas数据分析实战项目页"""
     return render_template('pandas-projects-page.html')
 
 
-@freezer.register_generator
-def url_generator():
-    yield '/'
-    yield '/index.html'
-    yield '/courses.html'
-    yield '/projects.html'
-    yield '/pandas-projects.html'
+if HAS_FREEZER:
+    @freezer.register_generator
+    def url_generator():
+        yield '/'
+        yield '/index.html'
+        yield '/courses.html'
+        yield '/projects.html'
+        yield '/pandas-projects.html'
 
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
+    if len(sys.argv) > 1 and sys.argv[1] == 'freeze' and HAS_FREEZER:
         freezer.freeze()
     else:
         app.run(debug=True, host='0.0.0.0', port=5000)
